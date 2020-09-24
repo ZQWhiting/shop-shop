45ff1fa066acbf5a283ab904d1c1ed32a15dbf50
ZQWhiting <zach.whiting@icloud.com>
2020-09-24 16:59:41 -0600

    implemented UPDATE_CART_QUANTITY on Detail and Home pages; implemented REMOVE_FROM_CART on Detail page;

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 
 import { useStoreContext } from '../utils/GlobalState';
@@ -10,43 +10,59 @@ import {
 	UPDATE_PRODUCTS,
 } from '../utils/actions';
 
-import { QUERY_PRODUCTS } from "../utils/queries";
-import spinner from '../assets/spinner.gif'
+import { QUERY_PRODUCTS } from '../utils/queries';
+import spinner from '../assets/spinner.gif';
 
 import Cart from '../components/Cart';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
-  const { id } = useParams();
+	const { id } = useParams();
+	const [state, dispatch] = useStoreContext();
+	const { products, cart } = state;
 
-  const [currentProduct, setCurrentProduct] = useState({})
+	const [currentProduct, setCurrentProduct] = useState({});
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+	const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+	const addToCart = () => {
+		const itemInCart = cart.find((cartItem) => cartItem._id === id);
 
-  const addToCart = () => {
+		if (itemInCart) {
+			dispatch({
+				type: UPDATE_CART_QUANTITY,
+				_id: id,
+				purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+			});
+		} else {
+			dispatch({
+				type: ADD_TO_CART,
+				product: { ...currentProduct, purchaseQuantity: 1 },
+			});
+		}
+	};
+
+	const removeFromCart = () => {
 		dispatch({
-			type: ADD_TO_CART,
-			product: { ...currentProduct, purchaseQuantity: 1 },
+			type: REMOVE_FROM_CART,
+			_id: currentProduct._id,
 		});
-  };
+	};
 
-  useEffect(() => {
-    // set current product to matching product in GlobalStore
+	useEffect(() => {
+		// set current product to matching product in GlobalStore
 		if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
+			setCurrentProduct(products.find((product) => product._id === id));
 
-    // if no products in GlobalStore, update GlobalStore and try again
+			// if no products in GlobalStore, update GlobalStore and try again
 		} else if (data) {
 			dispatch({
 				type: UPDATE_PRODUCTS,
 				products: data.products,
 			});
 		}
-  }, [products, data, dispatch, id]);
+	}, [products, data, dispatch, id]);
 
-  return (
+	return (
 		<>
 			{currentProduct ? (
 				<div className='container my-1'>
@@ -59,7 +75,10 @@ function Detail() {
 					<p>
 						<strong>Price:</strong>${currentProduct.price}{' '}
 						<button onClick={addToCart}>Add to Cart</button>
-						<button>Remove from Cart</button>
+						<button disabled={!cart.find(p => p._id === currentProduct._id)}
+						onClick={removeFromCart}>
+							Remove from Cart
+						</button>
 					</p>
 
 					<img
@@ -71,7 +90,7 @@ function Detail() {
 			{loading ? <img src={spinner} alt='loading' /> : null}
 			<Cart />
 		</>
-  );
-};
+	);
+}
 
 export default Detail;
