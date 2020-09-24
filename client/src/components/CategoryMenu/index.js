1d05e6a20cc13772ff4e20f751c1018168f12351
ZQWhiting <zach.whiting@icloud.com>
2020-09-24 15:18:29 -0600

    connected CategoryMenu to the GlobalStore

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from "../../utils/queries";
 
-function CategoryMenu({ setCategory }) {
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+import { useStoreContext } from '../../utils/GlobalState';
 
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map(item => (
-        <button
-          key={item._id}
-          onClick={() => {
-            setCategory(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+import {
+	UPDATE_CATEGORIES,
+	UPDATE_CURRENT_CATEGORY,
+} from '../../utils/actions';
+
+function CategoryMenu() {
+	const [state, dispatch] = useStoreContext();
+
+	const { categories } = state;
+
+	const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+	useEffect(() => {
+		// if categoryData exists or has changed from the response of useQuery
+		// then run dispatch() to update categories
+		if (categoryData) {
+			dispatch({
+				type: UPDATE_CATEGORIES,
+				categories: categoryData.categories,
+			});
+		}
+	}, [categoryData, dispatch]);
+
+	const handleClick = (id) => {
+		dispatch({
+			type: UPDATE_CURRENT_CATEGORY,
+			currentCategory: id,
+		});
+	};
+
+	return (
+		<div>
+			<h2>Choose a Category:</h2>
+			{categories.map((item) => (
+				<button
+					key={item._id}
+					onClick={() => {
+						handleClick(item._id);
+					}}
+				>
+					{item.name}
+				</button>
+			))}
+		</div>
+	);
 }
 
 export default CategoryMenu;
