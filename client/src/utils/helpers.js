9e6aeaa3e046027589c4be9446865a9fff5659ea
ZQWhiting <zach.whiting@icloud.com>
2020-09-28 18:44:50 -0600

    created idbPromise helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,61 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+	return new Promise((resolve, reject) => {
+		// open connection to database
+		const request = window.indexedDB.open('shop-shop', 1);
+
+		let db, tx, store;
+
+    // create object store
+		request.onupgradeneeded = function (e) {
+			const db = request.result;
+
+			db.createObjectStore('products', { keyPath: '_id' });
+			db.createObjectStore('categories', { keyPath: '_id' });
+			db.createObjectStore('cart', { keyPath: '_id' });
+		};
+
+		request.onerror = function (e) {
+			console.log('There was an error');
+		};
+
+		request.onsuccess = function (e) {
+      // open transaction from the database and get the object store
+			db = request.result;
+			tx = db.transaction(storeName, 'readwrite');
+			store = tx.objectStore(storeName);
+
+			db.onerror = function (e) {
+				console.log('error', e);
+			};
+
+      // perform CRUD method on object store
+			switch (method) {
+				case 'put':
+					store.put(object);
+					resolve(object);
+					break;
+				case 'get':
+					const all = store.getAll();
+					all.onsuccess = function () {
+						resolve(all.result);
+					};
+					break;
+				case 'delete':
+					store.delete(object._id);
+					break;
+				default:
+					console.log('No valid method');
+					break;
+			}
+
+      // close transaction
+			tx.oncomplete = function () {
+				db.close();
+			};
+		};
+	});
+}
